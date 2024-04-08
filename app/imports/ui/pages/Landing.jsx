c8f2192a042db78dcd9c349b8836982a3fb47be9
Ben <156398965+banilowben@users.noreply.github.com>
2024-04-07 15:56:30 -1000

    Landing Page and Added Logos
    Laid out the landing page and added logo images to /public/images/

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Container, Col, Row, Image } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
+  <Container id="landing-page" className="my-auto py-3 d-flex justify-content-center">
+    <Row className="d-flex justify-content-center">
+      <Col xs={3} className="text-center align-content-center">
+        <div>
+          <h2>Welcome</h2>
+          <p>
+            Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word.
+          </p>
+        </div>
+      </Col>
       <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+        <Image src="/images/logo-only.png" />
       </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
-
     </Row>
   </Container>
 );
