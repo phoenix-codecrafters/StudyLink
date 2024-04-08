fdb1be9ad63b94a24afa259f6eca16d412a14d6b
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-04-08 07:51:52 -1000

    1

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link, Navigate } from 'react-router-dom';
 import { Accounts } from 'meteor/accounts-base';
-import { Alert, Card, Col, Container, Row, Form } from 'react-bootstrap';
+import { Alert, Card, Col, Container, Row, Form, InputGroup } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, SubmitField, TextField, LongTextField } from 'uniforms-bootstrap5';
+import { PersonFill, LockFill, Image, Journal } from 'react-bootstrap-icons';
 
 /**
  * SignUp component is similar to signin component, but we create a new user instead.
@@ -19,14 +20,19 @@ const SignUp = ({ location }) => {
     password: String,
     firstname: String,
     lastname: String,
+    coursename: String,
+    image: String,
+    major: String,
+    subject: String,
+    description: String,
 
   });
   const bridge = new SimpleSchema2Bridge(schema);
 
   /* Handle SignUp submission. Create user account and a profile entry, then redirect to the home page. */
   const submit = (doc) => {
-    const { email, password, firstname, lastname } = doc;
-    Accounts.createUser({ email, username: email, password, firstname, lastname }, (err) => {
+    const { email, password, firstname, lastname, coursename, image, major, subject, description } = doc;
+    Accounts.createUser({ email, username: email, password, firstname, lastname, coursename, image, major, subject, description }, (err) => {
       if (err) {
         setError(err.reason);
       } else {
@@ -52,17 +58,67 @@ const SignUp = ({ location }) => {
           <AutoForm schema={bridge} onSubmit={data => submit(data)}>
             <Card>
               <Card.Body>
-                <TextField name="email" placeholder="E-mail address" />
-                <TextField name="password" placeholder="Password" type="password" />
+                <Form.Group className="mb-2">
+                  <Form.Label>Email</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <PersonFill />
+                    </InputGroup.Text>
+                    <Form.Control type="email" name="email" placeholder="E-mail address" />
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group className="mb-2">
+                  <Form.Label>Password</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <LockFill />
+                    </InputGroup.Text>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                  </InputGroup>
+                </Form.Group>
+
                 {/* First Name and Last Name in the same row */}
-                <Form.Group as={Row}>
+                <Form.Group as={Row} className="mb-2">
                   <Col>
-                    <TextField name="firstname" placeholder="First Name" />
+                    <Form.Label>First Name</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <PersonFill />
+                      </InputGroup.Text>
+                      <Form.Control type="text" name="firstname" placeholder="First Name" />
+                    </InputGroup>
                   </Col>
                   <Col>
-                    <TextField name="lastname" placeholder="Last Name" />
+                    <Form.Label>Last Name</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <PersonFill />
+                      </InputGroup.Text>
+                      <Form.Control type="text" name="lastname" placeholder="Last Name" />
+                    </InputGroup>
                   </Col>
                 </Form.Group>
+                <Form.Group className="mb-2">
+                  <Form.Label>Image</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <Image />
+                    </InputGroup.Text>
+                    <Form.Control type="text" name="image" placeholder="Image URL" />
+                  </InputGroup>
+                </Form.Group>
+                <TextField name="coursename" label="Course Name" placeholder="Course Name" />
+                <Form.Group className="mb-2">
+                  <Form.Label>Major</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <Journal />
+                    </InputGroup.Text>
+                    <Form.Control type="text" name="major" placeholder="Major" />
+                  </InputGroup>
+                </Form.Group>
+                <TextField name="subject" label="Subject" placeholder="Subject" />
+                <LongTextField name="description" label="Description" placeholder="Tell us more about you" component="textarea" rows={5} />
                 <ErrorsField />
                 <SubmitField value="Register" />
               </Card.Body>
