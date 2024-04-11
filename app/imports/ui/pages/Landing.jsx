3d40534e2657f926748f6aca843b48f3a1abca3d
Michelle4929 <giyoung7@hawaii.edu>
2024-04-10 18:29:59 -1000

    Updated Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,12 +4,18 @@ import { Container, Col, Row, Image } from 'react-bootstrap';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" className="my-auto py-3 d-flex justify-content-center">
-    <Row className="d-flex justify-content-center">
-      <Col xs={3} className="text-center align-content-center">
+    <Row className="d-flex justify-content-center" id="white-box">
+      <Col md={5} className="text-center align-content-center">
         <div>
-          <h2>Welcome</h2>
+          <h1><strong>Welcome To StudyLink</strong></h1>
           <p>
-            Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word. WORD. Word.
+            Nowadays, it’s pretty rare to see students interacting face-to-face with their peers. It’s tough to ask for help when everyone else seems to be doing just fine. But have no fear, because StudyLink is here to help!
+          </p>
+          <p>
+            Our web platform connects students with others who are studying the same things. You can collaborate on assignments, projects, and exam prep with your buddies. With StudyLink, joining study sessions has never been easier.
+          </p>
+          <p>
+            Simply log in or make an account to begin. Get started now!
           </p>
         </div>
       </Col>
