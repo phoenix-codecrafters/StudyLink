7adf87e77bcfa53fbdd983ab81753b559672c42c
Ben <156398965+banilowben@users.noreply.github.com>
2024-04-11 19:07:00 -1000

    Layout of Profile page
    react layout of profile page

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
   const user = Meteor.user();
 
   if (!user) {
-    return <div>How did you get here?... Go Back</div>; // or redirect to login page
+    return <div>How did you get here?... Go Back</div>;
   }
 
   return (
@@ -16,39 +16,51 @@ const Profile = () => {
           {/* Name */}
           <Col>
             <div className="d-flex flex-wrap">
-              <h2 className="">Name:</h2>
+              <h2 className="">First Name:</h2>
               <div className="my-2 d-flex">
-                <input className="mx-5 flex-grow-1" type="text" placeholder={user.username} disabled />
+                <input className="mx-5" type="text" placeholder="replace" disabled />
+              </div>
+            </div>
+            <div className="d-flex flex-wrap">
+              <h2 className="">Last Name:</h2>
+              <div className="my-2 d-flex">
+                <input className="mx-5" type="text" placeholder="replace" disabled />
+              </div>
+            </div>
+            <div className="d-flex flex-wrap">
+              <h2 className="">Email:</h2>
+              <div className="my-2 d-flex">
+                <input className="mx-5" type="text" placeholder={user.emails[0].address} disabled />
+              </div>
+            </div>
+            <div className="d-flex flex-wrap">
+              <h2 className="">Password:</h2>
+              <div className="my-2 d-flex">
+                <input className="mx-5" type="text" placeholder={user.password} disabled />
+              </div>
+            </div>
+            <div className="d-flex flex-wrap">
+              <h2 className="">Class Standing:</h2>
+              <div className="my-2 d-flex">
+                <input className="mx-5" type="text" placeholder="replace" disabled />
               </div>
             </div>
             <div className="d-flex flex-wrap">
               <h2 className="">Major:</h2>
               <div className="my-2 d-flex">
-                <input className="mx-5 flex-grow-1" type="text" disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Sensei:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5 flex-grow-1" type="text" disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Grasshopper:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5 flex-grow-1" type="text" disabled />
+                <input className="mx-5" type="text" placeholder="replace" disabled />
               </div>
             </div>
             <div className="d-flex flex-wrap">
               <h2 className="">Description:</h2>
               <div className="my-2 d-flex">
-                <textarea className="mx-5 description-box" rows="3" disabled />
+                <textarea className="mx-5 description-box" rows="3" placeholder="replace" disabled />
               </div>
             </div>
           </Col>
           <Col className="col-2">
             <div className="d-flex justify-content-end">
-              <Image className="pro-pic-image" src="https://avatars.githubusercontent.com/u/156398965?v=4" />
+              <Image className="pro-pic-image" src="https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=" />
             </div>
           </Col>
         </Row>
