16fa8c05cc61211384562eaaf04d3846782811ba
Ben <156398965+banilowben@users.noreply.github.com>
2024-04-17 00:06:24 -1000

    Updated edit page.
    Still need to have the submit method update the database. having permission errors

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Col, Row, Image, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Card, Col, Container, Row, Image } from 'react-bootstrap';
+import SimpleSchema from 'simpl-schema';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { AutoForm, ErrorsField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 
 const Profile = () => {
   const user = Meteor.user();
@@ -9,69 +14,79 @@ const Profile = () => {
     return <div>How did you get here?... Go Back</div>;
   }
 
+  const schema = new SimpleSchema({
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    image: String,
+    classStanding: {
+      type: String,
+      allowedValues: ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate'],
+    },
+    major: {
+      type: String,
+      allowedValues: ['Information and Computer Sciences (ICS)', 'Information Technology Management (ITM)', 'Computer Engineering'],
+    },
+    description: String,
+  });
+  const bridge = new SimpleSchema2Bridge(schema);
   return (
-    <Container id="profile-page" className="my-auto py-3 d-flex justify-content-center">
-      <Col className="col-7" id="white-box">
-        <Row className="mx-auto justify-content-start">
-          {/* Name */}
-          <Col>
-            <div className="d-flex flex-wrap">
-              <h2 className="">First Name:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5" type="text" placeholder="replace" disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Last Name:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5" type="text" placeholder="replace" disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Email:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5" type="text" placeholder={user.emails[0].address} disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Password:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5" type="text" placeholder={user.password} disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Class Standing:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5" type="text" placeholder={user.major} disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Major:</h2>
-              <div className="my-2 d-flex">
-                <input className="mx-5" type="text" placeholder="replace" disabled />
-              </div>
-            </div>
-            <div className="d-flex flex-wrap">
-              <h2 className="">Description:</h2>
-              <div className="my-2 d-flex">
-                <textarea className="mx-5 description-box" rows="3" placeholder="replace" disabled />
-              </div>
-            </div>
-          </Col>
-          <Col className="col-2">
-            <div className="d-flex justify-content-end">
-              <Image className="pro-pic-image" src="https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=" />
-            </div>
-          </Col>
-        </Row>
-        <Container className="pt-3 d-flex justify-content-end w-100">
-          <div>
-            <Button className="button-test">Edit</Button>
+    <Container id="signup-page" className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={12} md={6}>
+          <div className="text-center">
+            <h2>My Profile</h2>
           </div>
-        </Container>
-      </Col>
+          <AutoForm schema={bridge}>
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col>
+                    <TextField type="email" name="email" label="E-Mail:" value={user.emails[0].address} placeholder="E-mail address" disabled />
+                    <TextField type="password" name="password" label="Password:" value="**********" placeholder="Password" disabled />
+                  </Col>
+                  <Col xs={4} className="d-flex justify-content-center">
+                    <Image className="pro-pic-image" src={user.profile.image} />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <TextField type="text" name="firstname" label="First Name:" value={user.profile.firstname} placeholder="First Name" disabled />
+                  </Col>
+                  <Col>
+                    <TextField type="text" name="lastname" label="Last Name:" value={user.profile.lastname} placeholder="Last Name" disabled />
+                  </Col>
+                </Row>
+                <TextField type="text" name="image" label="Image Url:" value={user.profile.image} placeholder="Image URL" disabled />
+                <SelectField name="classStanding" label="Grade Status:" value={user.profile.classStanding} placeholder="Choose..." disabled>
+                  options= {['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate']}
+                </SelectField>
+                <SelectField label="Focus:" name="major" value={user.profile.major} placeholder="Choose..." disabled>
+                  options= {['Information and Computer Sciences (ICS)', 'Information Technology Management (ITM)', 'Computer Engineering']}
+                </SelectField>
+                <TextField as="textarea" name="description" label="Description:" value={user.profile.description} placeholder="Tell us more about you" disabled />
+                <ErrorsField />
+                <Link to="/editprofile">
+                  <SubmitField value="Edit" />
+                </Link>
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
     </Container>
   );
+};
+
+Profile.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.string,
+  }),
+};
+
+Profile.defaultProps = {
+  location: { state: '' },
 };
 
 export default Profile;
