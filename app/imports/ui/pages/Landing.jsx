5ac9e570df7675398599df00aa4270276bc556a4
Michelle4929 <giyoung7@hawaii.edu>
2024-04-22 15:07:55 -1000

    Updated Landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,11 +3,11 @@ import { Container, Col, Row, Image } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" className="my-auto py-3 d-flex justify-content-center">
-    <Row className="d-flex justify-content-center" id="white-box">
-      <Col md={5} className="text-center align-content-center">
+  <Container id="landing-page" className="my-auto py-5 d-flex justify-content-center">
+    <Row className="d-flex justify-content-center">
+      <Col md={5} className="custom-column text-center align-content-center">
         <div>
-          <h1><strong>Welcome To StudyLink</strong></h1>
+          <h1><strong>Welcome to StudyLink!</strong></h1>
           <p>
             Nowadays, it’s pretty rare to see students interacting face-to-face with their peers. It’s tough to ask for help when everyone else seems to be doing just fine. But have no fear, because StudyLink is here to help!
           </p>
@@ -19,8 +19,8 @@ const Landing = () => (
           </p>
         </div>
       </Col>
-      <Col xs={4}>
-        <Image src="/images/logo-only.png" />
+      <Col md={4} className="custom-column" id="white-box">
+        <Image src="https://www.regenesys.net/wp-content/uploads/2023/09/study-1.png" width="300px" />
       </Col>
     </Row>
   </Container>
