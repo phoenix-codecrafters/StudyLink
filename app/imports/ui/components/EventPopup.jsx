d4c6f8dd3900a4fad5f8adb9ca3cbf291ebab4ad
Andrew Gibbons <156730995+AndrewGibbons575@users.noreply.github.com>
2024-04-28 14:20:22 -1000

    Update EventPopup.jsx

--- app/imports/ui/components/EventPopup.jsx
+++ app/imports/ui/components/EventPopup.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import swal from 'sweetalert';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Button, Modal } from 'react-bootstrap';
+import { Sessions } from '../../api/session/Session';
+import LoadingSpinner from './LoadingSpinner';
 
 const EventPopup = ({ event, onClose }) => {
   // Convert the Date objects to string representations
@@ -14,7 +19,42 @@ const EventPopup = ({ event, onClose }) => {
   const startTime = startDate.slice(16, 24);
   const endTime = endDate.slice(16, 24);
 
-  return (
+  const user = Meteor.user();
+
+  const { doc, ready } = useTracker(() => {
+    // Get access to Contact documents.
+    const subscription = Meteor.subscribe(Sessions.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the document
+    const document = Sessions.collection.find({ _id: event.id }).fetch();
+    console.log(document);
+
+    return {
+      doc: document[0],
+      ready: rdy,
+    };
+  });
+
+  const rsvp = () => {
+    const username = user.username;
+    let inAttend = false;
+    doc.ghAttend.forEach(attendee => {
+      if (attendee === username) {
+        inAttend = true;
+      }
+    });
+    if (inAttend) {
+      swal('You are already attending this session!');
+    } else {
+      const ghAttend = [...doc.ghAttend, username]; // Use default empty array if ghAttend is undefined
+      Sessions.collection.update(doc._id, { $set: { ghAttend } }, (error) => (error ?
+        swal('Error', error.message, 'error') :
+        swal('Success', 'You have RSVP\'d to this session', 'success')));
+    }
+  };
+
+  return ready ? (
     <Modal show onHide={onClose}>
       <Modal.Header closeButton>
         <Modal.Title>{event.title}</Modal.Title>
@@ -24,14 +64,20 @@ const EventPopup = ({ event, onClose }) => {
         <p>Start: {startTime}</p>
         <p>End: {endTime}</p>
         <p>Description: {description}</p>
-        {/* Add more event details here as needed */}
+        <p>Sensei Students: </p>
+        <p>Grasshopper Students: {console.log(doc)} </p>
       </Modal.Body>
+      <Modal.Footer>
+        <Button style={{ backgroundColor: 'rgb(124, 209, 249)', borderColor: 'rgb(124, 209, 249)' }} onClick={rsvp}>RSVP</Button>
+      </Modal.Footer>
     </Modal>
-  );
+  ) : <LoadingSpinner />;
+
 };
 
 EventPopup.propTypes = {
   event: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     start: PropTypes.oneOfType([PropTypes.instanceOf(Date), PropTypes.string]).isRequired,
     end: PropTypes.oneOfType([PropTypes.instanceOf(Date), PropTypes.string]).isRequired,
