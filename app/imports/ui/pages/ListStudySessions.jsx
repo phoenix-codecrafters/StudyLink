6486d076acfc8e647b381bcd5d8096b7f5b5acc5
Ben <156398965+banilowben@users.noreply.github.com>
2024-04-28 17:34:35 -1000

    List My Study Sessions
    Converted StudySession Page to List my study session

--- app/imports/ui/pages/ListStudySessions.jsx
+++ app/imports/ui/pages/ListStudySessions.jsx
@@ -4,9 +4,10 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Sessions } from '../../api/session/Session';
-import StudySession from '../components/StudySession'; // Assuming StudySession component is defined
+import StudySession from '../components/StudySession';
 
 const ListStudySessions = () => {
+  const user = Meteor.user();
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, sessions } = useTracker(() => {
     // Note that this subscription will get cleaned up
@@ -21,28 +22,35 @@ const ListStudySessions = () => {
       sessions: sessionItems,
       ready: rdy,
     };
-  }, []);
+  });
 
-  return (ready ? (
-    <Container className="py-3">
-      <Row className="justify-content-center">
-        <Col>
-          <Col className="text-center">
-            <h2 className="page-title">List Study Sessions</h2>
+  const mySessions = sessions.filter(session => session.owner === user.username);
+
+  return (
+    ready ? (
+      <Container className="py-3">
+        <Row className="justify-content-center">
+          <Col>
+            <Col className="text-center">
+              <h2 className="mb-5">List My Study Sessions</h2>
+            </Col>
+            <Row xs={1} md={2} lg={3} className="g-4">
+              {mySessions.length === 0 ? (
+                <Container id="no-sessions-box"><h3>You currently don&apos;t own any sessions.</h3></Container>
+              ) : (
+                mySessions.map((session) => (
+                  <Col key={session._id}>
+                    <StudySession
+                      studySession={session}
+                    />
+                  </Col>
+                ))
+              )}
+            </Row>
           </Col>
-          <Row xs={1} md={2} lg={3} className="g-4">
-            {sessions.map((session) => (
-              <Col key={session._id}>
-                <StudySession
-                  studySession={session}
-                />
-              </Col>
-            ))}
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-  ) : <LoadingSpinner />);
+        </Row>
+      </Container>
+    ) : <LoadingSpinner />);
 };
 
 export default ListStudySessions;
